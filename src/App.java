d56b78ae287ec5992bb3681e42f6c69696113a97
dmiaper <dmiaper@hotmail.com>
2022-09-19 13:31:17 +0200

    hola

--- src/App.java
+++ src/App.java
@@ -1,52 +1,62 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        //Se declara variable para almacenar el resultado de la función.
-        int restultado;
-        //se ejecuta la funcón indicando el valor de los parametros a sumar y se obtiene el resultado.
-        restultado = suma(4,5,7);
-        //se muestra por pantalla el resultado de la operación realizada.
-        System.out.println(restultado);
+        //Se declaran las variables que se van a comparar.
+        int numeroIf= 0;
+        //bucle if-else para comprobar si el valor es mayor, igual o menor que 0
+        if(numeroIf>0){ 
+            System.out.println("El número "+numeroIf+" es mayor a cero");
+        }else if(numeroIf==0){ 
+            System.out.println("El número "+numeroIf+" es igual a cero");
+        }else{
+            System.out.println("El número "+numeroIf+" es menor a cero");
+        }
+       
+        //se declara la variable para el bucle while y do-while
+        int numeroWhile=0;
+        //bucle while para incrementar un número hasta llegar a un valor inferior a 3
+        while(numeroWhile<3){
+            System.out.println("El valor actual en el bucle While es " +numeroWhile);
+            numeroWhile++;
+        }
 
-        //se declara el objeto Coche
-        Coche miCoche = new Coche();
-        //se indica el valor de puertas actuales que tiene el Coche.
-        miCoche.ValorPuertasActual(2);
-        //se muestra por pantalla el valor de puertas que tiene el coche.
-        System.out.println("El número de puertas del coche es actualmente "+miCoche.puerta);
-        System.out.println("Se va a incrementar una puerta al coche de forma automática");
-        //se incrementa de formata automática en uno el valor de la puerta.
-        miCoche.IncrementarPuertas();
-        System.out.println("Ahora el coche tiene "+miCoche.puerta+" puertas");
+        //bucle do-while para incrementar un número
+        do{
+            System.out.println("El valor actual en el bucle do-while es " +numeroWhile);
+            numeroWhile++;
+        }while(numeroWhile<3);
+
+        //se declara la variable para el bucle for
+        int numeroFor =0;
+        //bucle for para incrementar un número hasta llegar a un valor inferior o igual a 3
+        for(int i=0;i<=3;i++){   
+            numeroFor = numeroFor+1;
+            System.out.println("El valor actual en en bucle FOR es " +numeroFor);
+        }
+                     
+
+        //se declara la variable estación
+        String estacion= "verano";
+
+        //bule switch para mostrar estación del año
+        switch(estacion){
+            case "invierno":
+                System.out.println("Ahora nos encontramos en la estación de Invierno");
+                break;
+            case "primavera":
+                System.out.println("Ahora nos encontramos en la estación de Primaverea");
+                break;
+            case "verano":
+                System.out.println("Ahora nos encontramos en la estación de Verano");
+                break;
+            case "otoño":
+                System.out.println("Ahora nos encontramos en la estación de Otoño");
+                break;
+            default:
+                System.out.println("No existe ninguna estación del año con ese nombre");
+                break;
         
+        }
     }
 
-    //se declara la función para realizar la suma de los paramatres y devuelve el resultado obtenido.
-    public static int suma (int a, int b, int c){
-        return a+b+c;
-    }
-
-}
-
-
-//se declara la Clase Coche con diferentes funciones y variables.
-class Coche{
-
-    //variable para almacenar el valor del número de puertas
-    public int puerta=0;
-    
-    //se declara la función para establecer en caso necesario el valor de puertas que tiene el coche actualmente.
-    public void ValorPuertasActual(int a){
-        this.puerta=a;
-    }
-
-    //se declara la función para incremetar de uno en uno el valor de las puertas.
-    public void IncrementarPuertas(){
-        this.puerta++;
-    }
-
-    //se declara variable para devolver el varlor acutal del número de puertas.
-    public int puerta(){
-        return this.puerta;
-    }
 }
